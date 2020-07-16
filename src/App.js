41f717219157d46842ea34bef431c63a7fe9b773
Lukas <lukas_weymann@hotmail.com>
2020-07-16 16:01:12 +0200

    Added gereral search input for the homepage as especified in the trello card

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
+import GeneralSearch from './components/SearchInput';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-    
+    <GeneralSearch/>
     </div>
   );
 }
