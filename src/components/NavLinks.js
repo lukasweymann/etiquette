a839358e8d458e40ee9c3e9e500775c4eb219ea0
Hanna Christ <christh87@googlemail.com>
2020-07-20 16:34:44 +0200

    Added pages to App.js and updated route for homepage

--- src/components/NavLinks.js
+++ src/components/NavLinks.js
@@ -1,20 +1,18 @@
 import React from "react";
-import { BrowserRouter as Router, Link } from "react-router-dom";
+import { BrowserRouter as Router, NavLink } from "react-router-dom";
 import "./NavLinks.css";
 import HomeIcon from "@material-ui/icons/Home";
 
 export default function NavLinks() {
   return (
     <div className="nav-links">
-    <Router>
-      <Link className="nav-links__singlelink homepage-link" to="homepage">
-        <HomeIcon />
-      </Link>
-      <Link className="nav-links__singlelink topics-link" to="topics">Topics</Link>
-      <Link className="nav-links__singlelink our-mission-link" to="our-mission">Our Mission</Link>
-      <Link className="nav-links__singlelink about-us-link" to="about-us">About Us</Link>
-      <Link className="nav-links__singlelink contact-link" to="contact">Contact</Link>
-      </Router>
+        <NavLink className="nav-links__singlelink homepage-link" exact to="/">
+          <HomeIcon />
+        </NavLink>
+        <NavLink className="nav-links__singlelink topics-link" to="topics">Topics</NavLink>
+        <NavLink className="nav-links__singlelink our-mission-link" to="our-mission">Our Mission</NavLink>
+        <NavLink className="nav-links__singlelink about-us-link" to="about-us">About Us</NavLink>
+        <NavLink className="nav-links__singlelink contact-link" to="contact">Contact</NavLink>
     </div>
   );
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import NavLinks from '../components/NavLinks';
 import GeneralSearch from '../components/SearchInput';
+import Logo from '../images/etiquette-logo.png';
 
 export default function HomePage(){
     return(
         <div className="homepage">
         <NavLinks/>
-        <img src="../../public/images/etiquette.png" alt="company's logo"/>
+        <img src={Logo} alt="company's logo"/>
         <GeneralSearch/>
         </div>
     );
