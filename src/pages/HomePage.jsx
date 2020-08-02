2e7c9b4ae0d31ae8ef7cef3b0d3fb376ace7772f
Hanna Christ <christh87@googlemail.com>
2020-08-02 17:38:34 +0200

    Started implementing curious button

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,25 @@
 import React, { useState } from "react";
+import { Link } from 'react-router-dom';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+
 import NavLinks from "../components/NavLinks";
 import CountrySelect from "../components/CountrySelect";
 import CuriousMapContainer from '../components/CuriousMapContainer';
 import "./HomePage.css";
 import Logo from "../images/etiquette-logo.png";
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+}));
+
 
 export default function HomePage() {
+  const classes = useStyles();
 
   return (
     <div className="homepage">
@@ -14,8 +27,18 @@ export default function HomePage() {
       <div className="homepage-container">
         <img src={Logo} className="etiquette-logo" alt="company's logo" />
         <CountrySelect/>
+        <div className={classes.root}>
+          <Button className="curious-button" variant="contained" component={Link} to="#CuriousMap">Are you curious?</Button>
+        </div>
+        <Link
+          to={{
+            hash: "#curious-map",
+          }}
+        >
+          Are you curious?
+        </Link>
         
-        <CuriousMapContainer />
+        <CuriousMapContainer id="CuriousMap"/>
       </div>
     </div>
   );
