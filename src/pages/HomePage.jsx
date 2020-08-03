1ef7c3bc66d0ca79ae0157685f2f4e27efa94c7e
Hanna Christ <christh87@googlemail.com>
2020-08-03 17:55:12 +0200

    Fixed curious-link style

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -27,6 +27,7 @@ export default function HomePage() {
           <CountrySelect />
           <Link
             className="homepage__curious-map-link"
+            
             to={{
               pathname: '/',
               hash: "#curious-map",
