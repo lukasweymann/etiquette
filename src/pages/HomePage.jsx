a2a2ff667a30765bf4d79f03d045d23c5d797d85
Hanna Christ <christh87@googlemail.com>
2020-08-03 11:55:39 +0200

    Updated homepage styles for map

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -25,26 +25,20 @@ export default function HomePage() {
         <div className="homepage-container">
           <img src={Logo} className="etiquette-logo" alt="company's logo" />
           <CountrySelect />
-        </div>
-    <div className="homepage">
-      <NavLinks />
-      <div className="homepage-container">
-        <img src={Logo} className="etiquette-logo" alt="company's logo" />
-        <CountrySelect/>
-        <Link
-          className="homepage__curious-map-link"
-          to={{
-            pathname: '/',
-            hash: "#curious-map",
-          }}
-        >
-          Are you curious?
+          <Link
+            className="homepage__curious-map-link"
+            to={{
+              pathname: '/',
+              hash: "#curious-map",
+            }}
+          >
+            Are you curious?
         </Link>
+        </div> 
+      </div>   
         
-        <CuriousMapContainer />
-      </div>
       <WhatsEtiquette />
-
+      <CuriousMapContainer />
       <Footer />
     </div>
   );
