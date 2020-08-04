2996e448c6810ff264ef3925726eef909a8d1a5c
Lukas <lukas_weymann@hotmail.com>
2020-08-04 10:09:01 +0200

    Finished the working register and login page. Styling included

--- src/pages/PremiumUserPage.jsx
+++ src/pages/PremiumUserPage.jsx
@@ -1,21 +1,31 @@
 import React from "react";
 import "./PremiumUserPage.css";
+
 import LoginComponent from "../components/LoginComponent";
 import RegisterComponent from "../components/RegisterComponent";
 import NavLinks from "../components/NavLinks";
 import Footer from "../components/Footer";
 import PremiumMembership from "../components/PremiumMembership";
+import { HashLink } from "react-router-hash-link";
 import Business from "../images/business.png";
 
 export default function PremiumUserPage() {
   return (
     <div className="premium-user__page">
-      <NavLinks />
-      <div className="premium-user__container">
-        <PremiumMembership />
+      <div className="premium-user-page__background">
+        <NavLinks />
 
-        <h1 className="premium-user-page__h1">My account</h1>
-        <div className="premium-user__components">
+        <PremiumMembership />
+        <HashLink
+          className="premium-user-page__cta"
+          to={{ pathname: "my-account", hash: "#premium-register" }}
+        >
+          GET YOUR MEMBERSHIP
+        </HashLink>
+      </div>
+      <div className="premium-user__container">
+        <h1 className="premium-user-page__h1" id="premium-register">My account</h1>
+        <div className="premium-user__components" >
           <LoginComponent />
           <RegisterComponent />
         </div>
